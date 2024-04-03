9c61b8dfcc684bc58bb36c13d946572ee0f57a61
TenorioNicolas <n.tenorio01@ufromail.cl>
2024-04-03 00:00:48 -0300

    Primer commit main

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Buenos Días mundo");
+        System.out.println("Nicolás Tenorio");
     }
 }
