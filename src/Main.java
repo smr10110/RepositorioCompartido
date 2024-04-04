323af11abd442505340140ac5ff7b8675c063cb3
TenorioNicolas <n.tenorio01@ufromail.cl>
2024-04-03 23:57:28 -0300

    Commit con imagen y merge

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hola Mundo");
         System.out.println("Buenos Días mundo");
         System.out.println("Nicolás Tenorio");
+        System.out.println("Angelo Huaiquil");
     }
 }
